2841e0c29a1a0c64306374afc3ee2ec06a99d5f0
Davi-S <67160664+Davi-S@users.noreply.github.com>
2026-07-21 01:41:54 -0300

    Add types and add future page in the main file

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,8 @@ import type { Schemas } from "../api/apiClient"
 
 export type Product = Schemas["ProductResponse"]
 export type Products = Product[]
+export type ProductCreateRequest = Schemas["ProductCreateRequest"]
+export type ProductUpdateRequest = Schemas["ProductUpdateRequest"]
 
 export type Stock = Record<string, number>
 export type PaymentType = Schemas["PaymentType"]
